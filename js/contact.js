392e16a034c3ad4582c5c1a8ce5ef578c47f299d
Sufyan <syedmuhammadsufyan237@gmail.com>
2024-11-07 06:52:05 +0500

    today

--- js/contact.js
+++ js/contact.js
@@ -34,3 +34,13 @@ phoneInputField.addEventListener('focus', function() {
         isCountryCodeAdded = false; // Allow adding country code again
     }
 });
+
+$(document).ready(function() {
+    $('button').on("click", function() {
+        $('.plane').addClass('fly');
+        $('.hidden').addClass('visible');
+        $('.replace').removeClass('fa-paper-plane').addClass('fa-check');
+        $('span').text('SENT').addClass('fade');
+        $(this).addClass('done');
+    });
+});
