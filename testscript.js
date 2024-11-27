155dfcf4014ae949dc80b26865b9dd0b73b9da7c
Muhammad Umair <muhammadumair25591@gamil.com>
2024-11-28 03:44:35 +0500

    change logo

--- testscript.js
+++ testscript.js
@@ -14,6 +14,13 @@ document.body.addEventListener('click', (ev) => {
 })
 
 button1.addEventListener('click', () => {
-  bg.spheres.setColors([0xffffff * Math.random(), 0xffffff * Math.random(), 0xffffff * Math.random()])
-  bg.spheres.light1.color.set(0xffffff * Math.random())
-})
+  // Generate random colors
+  const randomColor1 = 0xffffff * Math.random();
+  const randomColor2 = 0xffffff * Math.random();
+  const randomColor3 = 0xffffff * Math.random();
+  
+  bg.spheres.setColors([randomColor1, randomColor2, randomColor3]);
+  bg.spheres.light1.color.set(randomColor1);
+  
+  console.log('Random Colors:', randomColor1, randomColor2, randomColor3);
+});
