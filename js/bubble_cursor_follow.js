7b427582800f682fde81a4b82e87e6e774434a4e
Muhammad Umair <muhammadumair25591@gamil.com>
2024-12-06 00:46:19 +0500

    update footer

--- js/bubble_cursor_follow.js
+++ js/bubble_cursor_follow.js
@@ -13,14 +13,14 @@ document.body.addEventListener('click', (ev) => {
   if (ev.target !== button1) bg.togglePause()
 })
 
-button1.addEventListener('click', () => {
-  // Generate random colors
-  const randomColor1 = 0xffffff * Math.random();
-  const randomColor2 = 0xffffff * Math.random();
-  const randomColor3 = 0xffffff * Math.random();
+// button1.addEventListener('click', () => {
+//   // Generate random colors
+//   const randomColor1 = 0xffffff * Math.random();
+//   const randomColor2 = 0xffffff * Math.random();
+//   const randomColor3 = 0xffffff * Math.random();
   
-  bg.spheres.setColors([randomColor1, randomColor2, randomColor3]);
-  bg.spheres.light1.color.set(randomColor1);
+//   bg.spheres.setColors([randomColor1, randomColor2, randomColor3]);
+//   bg.spheres.light1.color.set(randomColor1);
   
-  console.log('Random Colors:', randomColor1, randomColor2, randomColor3);
-});
+//   console.log('Random Colors:', randomColor1, randomColor2, randomColor3);
+// });
