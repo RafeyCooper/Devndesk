70902340eb2dcab73db5ec77fb8274bdce419fe0
Muhammad Umair <muhammadumair25591@gamil.com>
2024-12-26 00:29:42 +0500

    push

--- js/test2.js
+++ js/test2.js
@@ -1,69 +1,71 @@
-const cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".glow-card");
 
-/**
- * return a value that has been rounded to a set precision
- * @param {Number} value the value to round
- * @param {Number} precision the precision (decimal places), default: 3
- * @returns {Number}
- */
-const round = (value, precision = 3) => parseFloat(value.toFixed(precision));
+(function () {
+    /**
+     * return a value that has been rounded to a set precision
+     * @param {Number} value the value to round
+     * @param {Number} precision the precision (decimal places), default: 3
+     * @returns {Number}
+     */
+    const round = (value, precision = 3) => parseFloat(value.toFixed(precision));
 
-/**
- * return a value that has been limited between min & max
- * @param {Number} value the value to clamp
- * @param {Number} min minimum value to allow, default: 0
- * @param {Number} max maximum value to allow, default: 100
- * @returns {Number}
- */
-const clamp = (value, min = 0, max = 100) => {
-    return Math.min(Math.max(value, min), max);
-};
+    /**
+     * return a value that has been limited between min & max
+     * @param {Number} value the value to clamp
+     * @param {Number} min minimum value to allow, default: 0
+     * @param {Number} max maximum value to allow, default: 100
+     * @returns {Number}
+     */
+    const clamp = (value, min = 0, max = 100) => {
+        return Math.min(Math.max(value, min), max);
+    };
 
-/**
- * return a value that has been re-mapped according to the from/to
- * - for example, adjust(10, 0, 100, 100, 0) = 90
- * @param {Number} value the value to re-map (or adjust)
- * @param {Number} fromMin min value to re-map from
- * @param {Number} fromMax max value to re-map from
- * @param {Number} toMin min value to re-map to
- * @param {Number} toMax max value to re-map to
- * @returns {Number}
- */
-const adjust = (value, fromMin, fromMax, toMin, toMax) => {
-    return round(
-        toMin + ((toMax - toMin) * (value - fromMin)) / (fromMax - fromMin)
-    );
-};
+    /**
+     * return a value that has been re-mapped according to the from/to
+     * - for example, adjust(10, 0, 100, 100, 0) = 90
+     * @param {Number} value the value to re-map (or adjust)
+     * @param {Number} fromMin min value to re-map from
+     * @param {Number} fromMax max value to re-map from
+     * @param {Number} toMin min value to re-map to
+     * @param {Number} toMax max value to re-map to
+     * @returns {Number}
+     */
+    const adjust = (value, fromMin, fromMax, toMin, toMax) => {
+        return round(
+            toMin + ((toMax - toMin) * (value - fromMin)) / (fromMax - fromMin)
+        );
+    };
 
-const cardUpdate = (e, $card) => {
-    // normalise touch/mouse
-    var pos = [e.clientX, e.clientY];
-    e.preventDefault();
-    if (e.type === "touchmove") {
-        pos = [e.touches[0].clientX, e.touches[0].clientY];
-    }
-    var dimensions = $card.getBoundingClientRect();
-    var l = pos[0] - dimensions.left;
-    var t = pos[1] - dimensions.top;
-    var h = dimensions.height;
-    var w = dimensions.width;
-    var px = clamp(Math.abs((100 / w) * l), 0, 100);
-    var py = clamp(Math.abs((100 / h) * t), 0, 100);
+    const cardUpdate = (e, $card) => {
+        // normalise touch/mouse
+        var pos = [e.clientX, e.clientY];
+        e.preventDefault();
+        if (e.type === "touchmove") {
+            pos = [e.touches[0].clientX, e.touches[0].clientY];
+        }
+        var dimensions = $card.getBoundingClientRect();
+        var l = pos[0] - dimensions.left;
+        var t = pos[1] - dimensions.top;
+        var h = dimensions.height;
+        var w = dimensions.width;
+        var px = clamp(Math.abs((100 / w) * l), 0, 100);
+        var py = clamp(Math.abs((100 / h) * t), 0, 100);
 
-    $card.setAttribute(
-        "style",
+        $card.setAttribute(
+            "style",
+            `
+          --pointer-x: ${px}%;
+          --pointer-y: ${py}%;
         `
-      --pointer-x: ${px}%;
-      --pointer-y: ${py}%;
-    `
-    );
-};
+        );
+    };
 
-// Loop through all cards and add the event listeners
-cards.forEach(($card) => {
-    $card.addEventListener("mousemove", (e) => cardUpdate(e, $card));
-    $card.addEventListener("touchmove", (e) => cardUpdate(e, $card));
-});
+    // Loop through all cards and add the event listeners
+    cards.forEach(($card) => {
+        $card.addEventListener("mousemove", (e) => cardUpdate(e, $card));
+        $card.addEventListener("touchmove", (e) => cardUpdate(e, $card));
+    });
+})();
 
 
 
